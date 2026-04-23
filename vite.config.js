7e64deade723c0d02aa406dde84beab014da8e62
anuj dubey <dubeyanuj411@gamil.com>
2026-04-23 11:09:53 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/",
+  // base: "/social-marketing/",
   plugins: [react(), tailwindcss()],
 });
